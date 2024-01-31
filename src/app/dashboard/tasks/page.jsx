ca284031f4a41c3502ac5629c92aef6886051c98
giannischiout <giannis.chiout@gmail.com>
2024-01-31 17:05:44 +0200

    ui: main layout ready

---
+++ src/app/dashboard/tasks/page.jsx
@@ -0,0 +1,15 @@
+
+import { DataTableDemo } from "@/app/_components/TableExample";
+const Page = () => {
+    return (
+        <>
+            <div className="mb-4">
+                <h2 className="text-3xl font-bold">Welcome back!</h2>
+                <p className="text-muted-foreground">Here's a list of your tasks for this month!</p>
+            </div>
+            < DataTableDemo />
+        </>
+    )
+}
+
+export default Page;
