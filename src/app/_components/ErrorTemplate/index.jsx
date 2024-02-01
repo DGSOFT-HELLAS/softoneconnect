41ad6865309d769d08be4b53ff2a134614f8c516
giannischiout <giannis.chiout@gmail.com>
2024-02-01 15:59:16 +0200

    ui: error template

---
+++ src/app/_components/ErrorTemplate/index.jsx
@@ -0,0 +1,34 @@
+
+'use client'
+import { AlertCircle } from 'lucide-react';
+import './styles.css'
+import { Button } from '@/components/ui/button';
+export default function ErrorTemplate({ error, reset }) {
+    return (
+        <main className="error_page">
+
+            <div className="error_container">
+                <div className="error_header">
+                    <AlertCircle />
+                    <h2 className="text-center">Looks like something went wrong!</h2>
+                </div>
+                <div className="error_content">
+                    <div className="error_message_container">
+                        <span>Error Message:</span>
+                        <span className="error_message">{error.message}</span>
+                    </div>
+                    <div className="error_btn">
+                        <Button
+                            onClick={() => reset()}>
+                            Try again
+                        </Button>
+                        <Button
+                            variant="outline"
+                        >home</Button>
+                    </div>
+                </div>
+            </div>
+
+        </main>
+    )
+}
