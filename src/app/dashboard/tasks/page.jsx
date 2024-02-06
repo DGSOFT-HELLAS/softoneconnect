6cea9905253fbeaeb44b5ca240b65478e93b158e
giannischiout <giannis.chiout@gmail.com>
2024-02-06 14:29:45 +0200

    build ready

--- src/app/dashboard/tasks/page.jsx
+++ src/app/dashboard/tasks/page.jsx
@@ -4,8 +4,8 @@ const Page = () => {
     return (
         <>
             <div className="mb-4">
-                <h2 className="text-3xl font-bold">Welcome back!</h2>
-                <p className="text-muted-foreground">Here's a list of your tasks for this month!</p>
+                <h2 className="text-3xl font-bold">{`Welcome back!`}</h2>
+                <p className="text-muted-foreground">{`Here's a list of your tasks for this month!`}</p>
             </div>
             < DataTableDemo />
         </>
