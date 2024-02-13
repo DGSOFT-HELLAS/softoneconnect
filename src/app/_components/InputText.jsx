cf1445eded338d45a223108c29b0437d9f856c3c
Genral Lee <dev@vmi1174119.contaboserver.net>
2024-02-13 10:28:16 +0200

    missing deps

--- src/app/_components/InputText.jsx
+++ src/app/_components/InputText.jsx
@@ -1,6 +1,13 @@
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
- 
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+  FormLabel,
+} from "@/components/ui/form"
 export function InputWithLabel({label, placeholder, control}) {
   return (
     <FormField
