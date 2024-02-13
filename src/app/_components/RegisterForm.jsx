a560dd25a87c7b2ed1aceb68820bb9474f4675fe
Genral Lee <dev@vmi1174119.contaboserver.net>
2024-02-13 16:43:26 +0200

    failed build

--- src/app/_components/RegisterForm.jsx
+++ src/app/_components/RegisterForm.jsx
@@ -1,5 +1,4 @@
 "use client"
-
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
@@ -8,17 +7,12 @@ import { Button } from "@/components/ui/button"
 import {
     Form,
     FormControl,
-    FormDescription,
     FormField,
     FormItem,
-    FormLabel,
     FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
-import { toast } from "@/components/ui/use-toast"
 import { Eye, EyeOff } from 'lucide-react';
-import axios from "axios"
-import { Crimson_Pro } from "next/font/google"
 
 const FormSchema = z.object({
     username: z.string().min(2, {
@@ -36,14 +30,7 @@ export default function RegisterForm() {
     })
 
     async function onSubmit(data) {
-        let url = "http://localhost:4000/api/test"
-        let url2 = "http://crm.cctracking.gr/api/test"
-        console.log(url)
-        console.log(url2)
-        const res = await axios.post(url);
-        const res2 = await axios.post(url2);
-        console.log(res.data)
-        console.log(res2.data)
+        console.log('submit')
 
     }
 
