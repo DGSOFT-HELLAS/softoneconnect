1f50eca5332443a4e45dbc5b2144b43594aafb69
giannischiout <giannis.chiout@gmail.com>
2024-02-14 13:59:42 +0200

    await user

--- src/app/api/auth/route.js
+++ src/app/api/auth/route.js
@@ -3,21 +3,22 @@ import connectMongo from '../../../../server/models/config';
 export async function POST(req, res) {
     
     const {email, password} = req.body;
+
     console.log(email, password )
     let user;
     try {
         await connectMongo()
-        user = User.create({
+        user = await User.create({
             email: email,
             password: password
         })
         console.log('user')
         console.log(user)
+       
     } catch (e) {
         throw new Error('something went wrong. User was not created')
     }
     return Response.json({
-        message: 'user created succesfully',
         user: user
     })
   }
