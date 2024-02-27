b7a600f5f1947a997a29be5cb9908786a479b8ad
giannischiout <giannis.chiout@gmail.com>
2024-02-27 14:41:09 +0200

    pie chart extra data

--- src/app/_components/TicketsPieChart/PieChart.jsx
+++ src/app/_components/TicketsPieChart/PieChart.jsx
@@ -43,8 +43,9 @@ const renderActiveShape = (props) => {
       />
       <path d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`} stroke={fill} fill="none" />
       <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
-      <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} textAnchor={textAnchor} fill="var(--primary)">{`Total Tickets: ${value}`}</text>
-      <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} dy={18} textAnchor={textAnchor} fill="var(--foreground)" className="text-sm mt-1">
+      <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey}  textAnchor={textAnchor} fill="var(--primary)">{payload['Χαρακτηρισμός']}</text>
+        <text className="text-sm mt-1" x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} dy={19} textAnchor={textAnchor} fill="var(--foreground)">{`Total Tickets: ${value}`}</text>
+      <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} dy={36} textAnchor={textAnchor} fill="var(--muted-foreground)" className="text-xs mt-1">
         {`(Rate ${(percent * 100).toFixed(2)}%)`}
       </text>
     </g>
