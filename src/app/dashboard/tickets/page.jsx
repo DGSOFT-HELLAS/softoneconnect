0341983d106a495bdaeb777b8a0694ca9686be58
giannischiout <giannis.chiout@gmail.com>
2024-02-27 15:25:55 +0200

    linechart

--- src/app/dashboard/tickets/page.jsx
+++ src/app/dashboard/tickets/page.jsx
@@ -5,62 +5,13 @@ import { getServerSession } from "next-auth/next"
 import { redirect } from 'next/navigation'
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import BarChart from "@/app/_components/BarChart";
-import Image from 'next/image';
 import TicketsPieChart from "@/app/_components/TicketsPieChart/PieChart";
-const images = [
-    'avatar1.jpg', 
-    'avatar1.jpg', 
-    'avatar1.jpg',
-    'avatar1.jpg',
-    'avatar1.jpg',
-    'avatar1.jpg',
-];
-
-function countTickets(tickets) {
-   
-    const agentTicketCounts = [];
-
-    tickets.forEach(ticket => {
-        // Get the agent name
-        const agent = ticket.ACTOR;
-        const initials = agent.split(' ').map(name => name[0]).join('');
-      
-        // Find the index of the agent in the agentTicketCounts array
-        const agentIndex = agentTicketCounts.findIndex(item => item.agent === agent);
-      
-        // If the agent is not in the agentTicketCounts array, add a new object
-        if (agentIndex === -1) {
-          agentTicketCounts.push({ agent, total: 1, initials });
-        } else {
-          // If the agent is already in the agentTicketCounts array, increment the total count
-          agentTicketCounts[agentIndex].total++;
-        }
-      });
-      return agentTicketCounts;
-}
+import TicketsLineChart from "@/app/_components/TicketsLineChart";
 
 
-function categorizeClasses(tickets) {
-    const classCounts = [];
 
-    tickets.forEach(ticket => {
-        // Get the agent name
-        const classification = ticket['Χαρακτηρισμός'];
-        console.log('classification')
-        console.log(classification)
-      
-        const index = classCounts.findIndex(item => item['Χαρακτηρισμός'] == classification);
-        console.log('index')
-        console.log(index)
-      
-        if (index === -1) {
-            classCounts.push({ 'Χαρακτηρισμός': classification, total: 1});
-        } else {
-          classCounts[index].total++;
-        }
-      });
-      return classCounts;
-}
+
+
 
 const fetchTickets = async () => {
     const response = await fetch("https://dgsoft.oncloud.gr/s1services/JS/ARIADNE/testCRMWebClient", {
@@ -78,13 +29,14 @@ const fetchTickets = async () => {
 
 const Page = async () => {
     const data = await fetchTickets();
-    // console.log(data)
+    console.log(data)
     const session = await getServerSession(authOptions);
 
     const ticketData = countTickets(data);
     const classifications = categorizeClasses(data);
-    console.log('classifications')
-    console.log(classifications)
+    console.log
+
+
 
     if (!session) {
         console.log('no session')
@@ -115,34 +67,74 @@ const Page = async () => {
                         <TicketsPieChart data={classifications}   />
                     </div>
                 </div>
+
                
 
+            </div>
+            <div className="mb-4 grid grid-cols-1">
+            <div className="bg-background  radius-md">
+                    <div className="mb-4 p-4 border-b text-muted-foreground border-background-main">
+                        <h2 className="text-lg  spacing-1">{"Tickets per agent"}</h2>
+                    </div>
+                    <div className="p-4 ">
+                        <TicketsLineChart   />
+                    </div>
+                </div>
             </div>
             <div className="mb-4">
                 <h2 className="text-3xl font-bold">{"Data Table"}</h2>
             </div>
-
             < TicketsTable data={data} />
         </>
     )
 }
 
 
-const CustomImageBar = ({ x, y, width, height, value, index }) => {
-    // Assuming you have an array of image URLs
-    return (
-      <g transform={`translate(${x},${y})`}>
-        <Image
-            style={{borderRadius: '50%'}}
-          x={width / 2 - 10} 
-          y={-10} 
-          width={20} 
-          height={20} 
-          src={images[index]}
-          alt={`Image ${index}`}
-        />
-      </g>
-    );
-  };
+function countTickets(tickets) {
+   
+    const agentTicketCounts = [];
+
+    tickets.forEach(ticket => {
+        // Get the agent name
+        const agent = ticket.ACTOR;
+        const initials = agent.split(' ').map(name => name[0]).join('');
+      
+        // Find the index of the agent in the agentTicketCounts array
+        const agentIndex = agentTicketCounts.findIndex(item => item.agent === agent);
+      
+        // If the agent is not in the agentTicketCounts array, add a new object
+        if (agentIndex === -1) {
+          agentTicketCounts.push({ agent, total: 1, initials });
+        } else {
+          // If the agent is already in the agentTicketCounts array, increment the total count
+          agentTicketCounts[agentIndex].total++;
+        }
+      });
+      return agentTicketCounts;
+}
+
+
+function categorizeClasses(tickets) {
+    const classCounts = [];
+
+    tickets.forEach(ticket => {
+        const classification = ticket['Χαρακτηρισμός'];
+        const index = classCounts.findIndex(item => item['Χαρακτηρισμός'] == classification);
+        if (index === -1) {
+            classCounts.push({ 'Χαρακτηρισμός': classification, total: 1, date: getRandomDate('2023-01-01', '2024-12-31')});
+        } else {
+          classCounts[index].total++;
+        }
+      });
+      return classCounts;
+}
+
+function getRandomDate(start, end) {
+    const startDate = new Date(start);
+    const endDate = new Date(end);
+    const randomDate = new Date(startDate.getTime() + Math.random() * (endDate.getTime() - startDate.getTime()));
+    return randomDate.toISOString(); // Convert to ISO string format
+  }
+  
 
 export default Page;
