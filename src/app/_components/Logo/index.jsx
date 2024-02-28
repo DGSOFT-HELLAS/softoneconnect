70ce315adb3621940ac5cb91c1cb8eea862cb588
giannischiout <giannis.chiout@gmail.com>
2024-02-28 14:20:59 +0200

    dghub

--- src/app/_components/Logo/index.jsx
+++ src/app/_components/Logo/index.jsx
@@ -1,7 +1,7 @@
 const Logo = () => {
     return (
       <div className='font-bold tracking-wide text-xl'>
-          <span>DIGI</span>
+          <span>DG</span>
           <span className='text-muted-foreground'>HUB</span>
           <span className='text-primary'>.</span>
       </div>
---
+++ src/app/dashboard/custom-report/page.jsx
@@ -0,0 +1,8 @@
+import CustomReport from "@/app/_components/CustomReportTable"
+export default function Page() {
+    return (
+        <div>
+            < CustomReport />
+        </div>
+    )
+}
