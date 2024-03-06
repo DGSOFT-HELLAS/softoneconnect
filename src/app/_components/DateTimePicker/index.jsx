b5f11b91f6104f4750c3bead2730badfaeb863e3
giannischiout <giannis.chiout@gmail.com>
2024-03-06 12:51:14 +0200

    ui: 0 prefix when time is a single unit

--- src/app/_components/DateTimePicker/index.jsx
+++ src/app/_components/DateTimePicker/index.jsx
@@ -14,11 +14,11 @@ import { format } from "date-fns";
 
 
 export default function DateTime({ label, onChange, selectedDate }) {
-	const [date, setDate] =useState(new Date())
+	const [date, setDate] = useState(new Date())
 
 	const [state, setState] = useState({
-		hour: 12,
-		minutes: 0,
+		hour: '12',
+		minutes: '00',
 		date: selectedDate,
 	})
 
@@ -27,28 +27,40 @@ export default function DateTime({ label, onChange, selectedDate }) {
 		setState(prev => ({ ...prev, startDate: selectedDate, endDate: selectedDate }))
 	}, [])
 
+
+	// const cleanTimeInputs = (e) => {
+	// 	if
+	// 	return;
+	// }
+
 	const handleIncreaseInterval = (interval, name, limit) => {
+		
 		setState(prev => {
-			if(prev[name] + interval >  limit) {
+			if (parseInt(prev[name]) + interval > limit) {
 				return {
 					...prev
 				}
 			};
+
+			let increase = parseInt(prev[name]) + interval;
+			increase = increase < 10 ? `0${increase}` : increase.toString();
 			return {
-				...prev, [name]: prev[name] + interval 
+				...prev, [name]: increase
 			}
 		})
 	}
 
 	const handleDecreaseInterval = (interval, name, limit) => {
 		setState(prev => {
-			if(prev[name]  ==  limit) {
+			if (parseInt(prev[name]) == limit) {
 				return {
 					...prev
 				}
 			};
+			let increase = parseInt(prev[name]) - interval;
+			increase = increase < 10 ? `0${increase}` : increase.toString();
 			return {
-				...prev, [name]: prev[name] - interval 
+				...prev, [name]: increase
 			}
 		})
 	}
@@ -57,20 +69,19 @@ export default function DateTime({ label, onChange, selectedDate }) {
 	const handleChange = (e) => {
 		const name = e.target.name;
 		let value = e.target.value;
-		
+
 		setState(prev => ({ ...prev, [name]: parseInt(value) }))
 	}
 
 	const handleSelectDate = (e) => {
 		let formated = format(e, "yyyy-MM-dd");
-		setState(prev => ({...prev, date: formated  }))
+		setState(prev => ({ ...prev, date: formated }))
 	}
 	return (
 		<Popover>
-		<div className="w-full ">
-			
-			<Label htmlFor="message">{label}</Label>
-			<div className={styles.inputContainer}>
+			<div className="w-full ">
+				<Label htmlFor="message">{label}</Label>
+				<div className={styles.inputContainer}>
 					<PopoverTrigger asChild>
 						<div className={styles.hourContainer}>
 							<CalendarIcon />
@@ -80,41 +91,41 @@ export default function DateTime({ label, onChange, selectedDate }) {
 						</div>
 					</PopoverTrigger>
 					<PopoverContent className="w-auto p-0 mt-4" align="start">
-						<Calendar 
+						<Calendar
 							mode="single"
 							locale={el}
 							onSelect={handleSelectDate}
 						/>
 					</PopoverContent>
-				{/* Single Input */}
-				<div className={styles.timeContainer}>
-					<div className={styles.timeInput}>
-						<input name="hour" onChange={handleChange} type="number" className={styles.timeSmallInput} value={state.hour} />
-						<div className={styles.arrows}>
-							<button onClick={() => handleIncreaseInterval(1, 'hour', 24)}>
-								<ChevronUp />
-							</button>
-							<button onClick={() => handleDecreaseInterval(1, 'hour', 0)}>
-								<ChevronDown  />
-							</button>
-						</div>
-					</div>
-					<span>:</span>
 					{/* Single Input */}
-					<div className={styles.timeInput}>
-						<input name="minutes" onChange={handleChange} type="number" className={styles.timeSmallInput} value={state.minutes} />
-						<div className={styles.arrows}>
-							<button onClick={() => handleIncreaseInterval(5, 'minutes', 60)}>
-								<ChevronUp />
-							</button>
-							<button onClick={() => handleDecreaseInterval(5, 'minutes', 0)}>
-								<ChevronDown />
-							</button>
+					<div className={styles.timeContainer}>
+						<div className={styles.timeInput}>
+							<input name="hour" onChange={handleChange} type="number" className={styles.timeSmallInput} value={state.hour} />
+							<div className={styles.arrows}>
+								<button onClick={() => handleIncreaseInterval(1, 'hour', 24)}>
+									<ChevronUp />
+								</button>
+								<button onClick={() => handleDecreaseInterval(1, 'hour', 0)}>
+									<ChevronDown />
+								</button>
+							</div>
+						</div>
+						<span>:</span>
+						{/* Single Input For setting time */}
+						<div className={styles.timeInput}>
+							<input name="minutes" onChange={handleChange} type="text" className={styles.timeSmallInput} value={state.minutes} />
+							<div className={styles.arrows}>
+								<button onClick={() => handleIncreaseInterval(5, 'minutes', 60)}>
+									<ChevronUp />
+								</button>
+								<button onClick={() => handleDecreaseInterval(5, 'minutes', 0)}>
+									<ChevronDown />
+								</button>
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div>
 		</Popover>
 	)
 }
